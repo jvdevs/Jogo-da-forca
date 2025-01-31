f39a504bb6f648592874b59fe3a72295c8b79189
jvdevs <joaodarklegal@gmail.com>
2025-01-31 13:30:44 -0300

    Cores e fonte

--- main.py
+++ main.py
@@ -7,3 +7,11 @@ pygame.init()
 LARGURA, ALTURA = 600, 400
 tela = pygame.display.set_mode((LARGURA, ALTURA))
 pygame.display.set_caption("Jogo da Forca")
+
+
+BRANCO = (255, 255, 255)
+PRETO = (0, 0, 0)
+VERMELHO = (255, 0, 0)
+
+
+fonte = pygame.font.Font(None, 36)
